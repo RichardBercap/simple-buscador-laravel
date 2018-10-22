03c91e84bcb0dad59cdd206535b6112c1d796598
RichardBercap <bercap211@gmail.com>
2018-10-21 20:48:21 -0400

    first commit

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -10,11 +10,11 @@ class Usercontroller extends Controller
 {
     public function index(Request $request)
     {
-      $name = $request->get('name');
-      $email = $request->get('email');
-      $bio = $request->get('bio');
+       $name = $request->get('name');
+       $email = $request->get('email');
+       $bio = $request->get('bio');
 
-      $users = User::orderBy('id','DESC')
+       $users = User::orderBy('id','DESC')
                   ->email($email)
                   ->name($name)
                   ->bio($bio)
